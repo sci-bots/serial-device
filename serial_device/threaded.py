fb2fd3357881814a189545ac276c6979858f7bd5
Christian Fobel <christian@fobel.net>
2017-06-23 03:13:03 -0400

    Refactor keep_alive into KeepAlive thread class
    Refactor `serial_device.threaded.keep_alive` function into
    `serial_device.threaded.KeepAlive` thread class.

--- serial_device/threaded.py
+++ serial_device/threaded.py
@@ -48,7 +48,7 @@ class EventProtocol(serial.threaded.Protocol):
             print 'Connection to port `%s` lost' % self.port
 
 
-def keep_alive(state, protocol_class, comport, **kwargs):
+class KeepAliveReader(threading.Thread):
     '''
     Keep a serial connection alive (as much as possible).
 
@@ -62,29 +62,115 @@ def keep_alive(state, protocol_class, comport, **kwargs):
         Keyword arguments passed to ``serial_for_url`` function, e.g.,
         ``baudrate``, etc.
     '''
-    if comport not in serial_device.comports().index:
-        raise NameError('Port `%s` not available.  Available ports: `%s`' %
-                        (comport, ', '.join(serial_device.comports().index)))
-    while True:
-        while comport not in serial_device.comports().index:
-            time.sleep(2)
-        try:
-            device = serial.serial_for_url(comport, **kwargs)
-        except serial.SerialException:
-            pass
-        else:
-            with serial.threaded.ReaderThread(device,
-                                              protocol_class) as protocol:
-                state['protocol'] = protocol
+    def __init__(self, protocol_class, comport, **kwargs):
+        super(KeepAliveReader, self).__init__()
+        self.daemon = True
+        self.protocol_class = protocol_class
+        self.comport = comport
+        self.kwargs = kwargs
+        self.protocol = None
+        # Event to indicate serial connection has been established.
+        self.connected = threading.Event()
 
-                # Wait for connection.
-                protocol.connected.wait()
-                # Wait for disconnection.
-                protocol.disconnected.wait()
+    def run(self):
+        # Verify requested serial port is available.
+        if self.comport not in serial_device.comports().index:
+            raise NameError('Port `%s` not available.  Available ports: `%s`' %
+                            (self.comport,
+                             ', '.join(serial_device.comports().index)))
+        while True:
+            # Wait for requested serial port to become available.
+            while self.comport not in serial_device.comports().index:
+                # Assume serial port was disconnected temporarily.  Wait and
+                # periodically check again.
+                time.sleep(2)
+            try:
+                # Try to open serial device and monitor connection status.
+                device = serial.serial_for_url(self.comport, **self.kwargs)
+            except serial.SerialException:
+                pass
+            else:
+                with serial.threaded.ReaderThread(device, self
+                                                  .protocol_class) as protocol:
+                    self.protocol = protocol
+
+                    # Wait for connection.
+                    protocol.connected.wait()
+                    self.connected.set()
+                    # Wait for disconnection.
+                    protocol.disconnected.wait()
+                    self.connected.clear()
+                    # Loop to try to reconnect to serial device.
+
+    def write(self, data, timeout_s=None):
+        '''
+        Write to serial port.
+
+        Waits for serial connection to be established before writing.
+
+        Parameters
+        ----------
+        data : str or bytes
+            Data to write to serial port.
+        timeout_s : float, optional
+            Maximum number of seconds to wait for serial connection to be
+            established.
+
+            By default, block until serial connection is ready.
+        '''
+        self.connected.wait(timeout_s)
+        self.protocol.transport.write(data)
+
+    def request(self, response_queue, payload, timeout_s=None,
+                poll=POLL_QUEUES):
+        '''
+        Send
+
+        Parameters
+        ----------
+        device : serial.Serial
+            Serial instance.
+        response_queue : Queue.Queue
+            Queue to wait for response on.
+        payload : str or bytes
+            Payload to send.
+        timeout_s : float, optional
+            Maximum time to wait (in seconds) for response.
+
+            By default, block until response is ready.
+        poll : bool, optional
+            If ``True``, poll response queue in a busy loop until response is
+            ready (or timeout occurs).
+
+            Polling is much more processor intensive, but (at least on Windows)
+            results in faster response processing.  On Windows, polling is
+            enabled by default.
+        '''
+        self.connected.wait(timeout_s)
+        return request(self, response_queue, payload, timeout_s=timeout_s,
+                       poll=poll)
+
+    # - -  context manager, returns protocol
+
+    def __enter__(self):
+        """\
+        Enter context handler. May raise RuntimeError in case the connection
+        could not be created.
+        """
+        self.start()
+        # Wait for protocol to connect.
+        self.connected.wait()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Leave context: close port"""
+        self.close()
 
 
 def request(device, response_queue, payload, timeout_s=None, poll=POLL_QUEUES):
     '''
+    Send payload to serial device and wait for response.
+
     Parameters
     ----------
     device : serial.Serial
@@ -98,12 +184,12 @@ def request(device, response_queue, payload, timeout_s=None, poll=POLL_QUEUES):
 
         By default, block until response is ready.
     poll : bool, optional
-        If ``True``, poll response queue in a busy loop until response is ready
-        (or timeout occurs).
+        If ``True``, poll response queue in a busy loop until response is
+        ready (or timeout occurs).
 
         Polling is much more processor intensive, but (at least on Windows)
-        results in faster response processing.  On Windows, polling is enabled
-        by default.
+        results in faster response processing.  On Windows, polling is
+        enabled by default.
     '''
     device.write(payload)
     if poll:
